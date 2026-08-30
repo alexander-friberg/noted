ffe3e6bed53bce2ecaced18219331db7a4895846
Alexander Friberg <alexander.friberg.dev@proton.me>
2026-08-31 01:18:21 +0200

    feat(notelist): remove useState

--- src/app/components/NoteList.tsx
+++ src/app/components/NoteList.tsx
@@ -1,18 +1,13 @@
-"use client"
-import { prisma } from "@/lib/prisma"
-import { useState } from "react"
 import { Note } from "@/generated/prisma/browser"
 
-
 export function NoteList({userNotes, user} : {userNotes: Note[]; user: string}) {
-  const [notes, setNotes] = useState<Note[]>(userNotes)
 
   return(
     <div className="flex flex-col items-start py-4 px-1 gap-2 w-full ">
-      {notes.map((note, i) => (
+      {userNotes.map((note, _) => (
         <div className="flex flex-row w-full h-fit font-salma 
           items-center text-nowrap gap-2 p-2 justify-between 
-          border-2 border-white transition duration-150 hover:-translate-x-1">
+          border-2 border-[--foreground] transition duration-150 hover:-translate-x-1">
           <div className="flex gap-2 items-center">
             <span>{note.title}</span>
             <span className="text-gray-400 text-sm">by {user}</span>
