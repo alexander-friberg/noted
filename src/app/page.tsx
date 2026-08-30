4d9d45aceb32511780bb3fb0b06f640a34440861
Alexander Friberg <alexander.friberg.dev@proton.me>
2026-08-31 01:17:39 +0200

    feat(page): extract date into an arrow function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,18 @@ import { getUserWithNotes, UserWithNotes } from "@/actions/user-actions";
 export default async function Home() {
   const date = new Date()
   const user: UserWithNotes = await getUserWithNotes()
+  const formatDate = () => `${date.getDate()} ${Object.values(Months).at(date.getMonth())} ${date.getFullYear()}`
+
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-start py-12 px-12 bg-white dark:bg-black sm:items-start">
-        <div className=" flex flex-col justify-start border-b-2 border-b-white pb-4">
+        <div className=" flex flex-col justify-start border-b-2 border-[--foreground] pb-4">
           <h1 className="font-salma text-4xl ">NOTED</h1>
-          <span className="text-gray-300">{date.getDate()} {Object.values(Months).at(date.getMonth())} {date.getFullYear()}</span>
+          <span className="text-gray-600 dark:text-gray-400">{formatDate()}</span>
         </div>
         <NoteList userNotes={user.notes} user={user.name}/>
       </main>
     </div>
   );
 }
+
