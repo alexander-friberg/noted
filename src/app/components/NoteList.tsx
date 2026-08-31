5b6c65ae9000e5642e22f20e21b89303afaf900c
Alexander Friberg <alexander.friberg.dev@proton.me>
2026-08-31 03:53:28 +0200

    feat(notelist): add Link for NotePage endpoint

--- src/app/components/NoteList.tsx
+++ src/app/components/NoteList.tsx
@@ -1,19 +1,23 @@
 "use client"
 import { Note } from "@/generated/prisma/browser"
 import { Footer, Text} from "./ui/text";
+import Link from "next/link";
 
 export function NoteList({userNotes, user} : {userNotes: Note[]; user: string}) {
 
   return(
     <div className="flex flex-col items-start py-4 px-1 gap-2 w-full ">
       {userNotes.map((note, _) => (
+      <Link href={`note/${note.id}`}
+        className="flex w-full h-full">
         <div className="flex flex-row w-full h-fit items-center text-nowrap gap-2 p-2 justify-between border-2 border-foreground transition duration-150 hover:-translate-x-1">
-          <div className="flex gap-2 items-center">
+          <div className="flex gap-4 items-center">
             <Text>{note.title}</Text>
             <Footer>by {user}</Footer>
           </div>
           <Footer>{note.createdAt.toLocaleDateString()}</Footer>
         </div>
+      </Link>
       ))}
     </div>
   )
